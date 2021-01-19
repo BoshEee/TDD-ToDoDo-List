830b1131f2e7cdabe7eb714235885ba3314f340f
Haneen Awad <jamiecoe66@gmail.com>
2021-01-19 14:48:52 +0200

    added

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-
+let textInput = document.querySelector(".todo-input");
+let toDoList = document.querySelector(".todo-list");
+let addBtn = document.querySelector(".todo-btn");
+addBtn.addEventListener("click", add);
+function add(event) {
+  event.preventDefault();
+  let toDoDiv = document.createElement("div");
+  let newLi = document.createElement("li");
+  let compBtn = document.createElement("button");
+  let trashBtn = document.createElement("button");
+  newLi.textContent = textInput.value;
+  toDoList.appendChild(toDoDiv);
+  toDoDiv.appendChild(newLi);
+  toDoDiv.appendChild(compBtn);
+  toDoDiv.appendChild(trashBtn);
+}
