0f3dbc515a51391597e9ffa5ef88012a8c6c0431
Haneen Awad <jamiecoe66@gmail.com>
2021-01-20 09:16:30 +0200

    add

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ let textInput = document.querySelector(".todo-input");
 let toDoList = document.querySelector(".todo-list");
 let addBtn = document.querySelector(".todo-btn");
 addBtn.addEventListener("click", add);
+
+function check(event) {
+  event.preventDefault();
+  event.target.style.textDecoration = "line-through";
+  //event.target.classList.add("checkstyle");
+}
+
 function add(event) {
   event.preventDefault();
   let toDoDiv = document.createElement("div");
   let newLi = document.createElement("li");
   let compBtn = document.createElement("button");
+  compBtn.addEventListener("click", check);
+  compBtn.textContent = "check";
   let trashBtn = document.createElement("button");
+
   newLi.textContent = textInput.value;
   toDoList.appendChild(toDoDiv);
   toDoDiv.appendChild(newLi);
--- index.test.js
+++ index.test.js
@@ -1,11 +1,14 @@
 //Haneen - Add tasks to a list so that I can keep track of them
+
 test("Submitting a new task adds it to the list", (t) => {
   let aInput = document.querySelector(".todo-input");
   aInput.value = "work";
   let submitButton = document.querySelector(".todo-btn");
   submitButton.click();
   let result = document.querySelector(".todo-list");
-  t.equal(result.textContent, "work");
+  console.log(result.textContent);
+  t.equal(result.firstChild.firstChild.textContent, "work");
+
   result.textContent = "";
 });
 
@@ -15,6 +18,51 @@ test("Submitting a new task adds it to the list", (t) => {
   let submitButton = document.querySelector(".todo-btn");
   submitButton.click();
   let result = document.querySelector(".todo-list");
-  t.equal(result.textContent, "study");
+  t.equal(result.firstChild.firstChild.textContent, "play");
+
+  aInput.value = "study";
+  submitButton.click();
+  t.equal(result.lastChild.firstChild.textContent, "study");
   result.textContent = "";
 });
+
+// test("Submitting a new task adds it to the list", (t) => {
+//   let aInput = document.querySelector(".todo-input");
+//   aInput.value = "work";
+//   let submitButton = document.querySelector(".todo-btn");
+//   submitButton.click();
+//   let result = document.querySelector(".todo-list");
+//   t.equal(result.textContent, "work");
+//   result.textContent = "";
+// });
+
+// test("Submitting a new task adds it to the list", (t) => {
+//   let aInput = document.querySelector(".todo-input");
+//   aInput.value = "play";
+//   let submitButton = document.querySelector(".todo-btn");
+//   submitButton.click();
+//   let result = document.querySelector(".todo-list");
+//   t.equal(result.textContent, "study");
+//   result.textContent = "";
+// });
+
+// test("Submitting a new task adds it to the list", (t) => {
+//   let aInput = document.querySelector(".todo-input");
+//   let submitButton = document.querySelector(".todo-btn");
+//   let list = document.querySelector(".todo-list");
+
+//   aInput.value = "Work";
+//   submitButton.click();
+
+//   const todoItem = document.createElement("li");
+//   todoItem.textContent = aInput.value;
+//   list.appendChild(todoItem);
+
+//   const actual = list.lastChild.textContent;
+//   const expected = "Work";
+
+//   t.equal(actual, expected);
+
+//   list.textContent = "";
+//   aInput.value = "";
+// });
